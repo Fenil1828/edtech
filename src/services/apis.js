e334426c346b10c1ab36dd19e2b27903da3adae7
Fenil1828 <fenil@example.com>
2026-04-20 18:11:02 +0530

    Update backend URL to Vercel deployment

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "https://study-notion38-b.vercel.app/api/v1"
-const BASE_URL = "https://studynotion38-b6.onrender.com/api/v1"
+const BASE_URL = "https://edtech-b.vercel.app/api/v1"
 
 
 export const categories = {
